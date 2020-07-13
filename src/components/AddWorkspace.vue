c4d6fbe7cd9b2b43e4a288db60fe4d8aacbbabf7
Moon Juhan <codemike@naver.com>
2020-07-13 22:01:09 +0900

    feat - 프로젝트 생성 페이지 추가

--- src/components/AddWorkspace.vue
+++ src/components/AddWorkspace.vue
@@ -1,43 +1,73 @@
 <template>
-    <div class="work-space-form">
-        <q-input v-model="title" label="title"/>
-        <q-input v-model="description" label="description" autogrow />
-        <add-members />
-    </div>
+  <div class="work-space-form">
+    <q-input v-model="title" label="title" />
+    <q-input v-model="description" label="description" autogrow />
+
+    <q-form class="search-form" @submit="searchMember">
+      <q-input ref="keyword" v-model="keyword" placeholder="검색어를 입력해주세요." />
+
+      <div class="search-form__button">
+        <q-btn flat label="Search" color="blue" type="submit" />
+      </div>
+    </q-form>
+
+    <add-member-card></add-member-card>
+    <!-- Dialog -->
+    <q-dialog v-model="dialogOpened">
+      <div>
+        <div></div>
+        <div></div>
+        <div>
+          <q-btn label="Cancle" v-close-popup />
+          <q-btn label="Confirm" />
+        </div>
+      </div>
+    </q-dialog>
+  </div>
 </template>
 
 <script>
-import AddMembers from "./AddMembers.vue";
+import AddMemberCard from "./AddMemberCard.vue";
 
 export default {
-    name: "add-workspace",
-    components: {
-        AddMembers
+  name: "add-workspace",
+  components: {
+    AddMemberCard
+  },
+  data() {
+    return {
+      title: "",
+      description: "",
+      members: [],
+      keyword: "",
+      dialogOpened: false
+    };
+  },
+  methods: {
+    addingMember() {
+      this.$children
+        .find(el => el.vueName == "add-members")
+        .addMember(res => {
+          this.members.push(res);
+        });
     },
-    data() {
-        return {
-            title: "",
-            description: "",
-            members: []
-        };
-    },
-    methods: {
-        addingMember() {
-            this.$children
-                .find(el => el.vueName == "add-members")
-                .addMember(res => {
-                    this.members.push(res);
-                });
-        },
+    searchMember() {
+      console.log(this.keyword);
+      this.dialogOpened = true;
     }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .work-space-form {
-    width: 100px;
-    .qinput {
-        width: 300px;
+  width: 30%;
+  .search-form {
+    display: flex;
+    justify-content: space-between;
+    &__button {
+      height: 100%;
     }
+  }
 }
 </style>
--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,13 +1,37 @@
 <template>
-  <div>프로젝트 리스트</div>
+  <div class="project-list">
+    <workspace-card :clickAction="action1" v-for="(item, index) in projects" :key="index">
+      <template v-slot:card-title>{{item.title}}</template>
+      <template v-slot:card-description>{{item.description}}</template>
+      <template v-slot:card-action></template>
+    </workspace-card>
+    <workspace-card :clickAction="addProject">
+      <template v-slot:card-title>+</template>
+    </workspace-card>
+  </div>
 </template>
 
 <script>
 import { T } from "../store/module/types.js";
+import WorkspaceCard from "../components/WorkspaceCard";
 
 export default {
   data() {
-    return {};
+    return {
+      projects: [
+        {
+          title: "test1",
+          description: "설명입니다."
+        },
+        {
+          title: "test2",
+          description: "설명입니다."
+        }
+      ]
+    };
+  },
+  components: {
+    WorkspaceCard
   },
   mounted() {
     this.$store.dispatch(T.CHANGE_TOP_STYLE, "");
@@ -25,10 +49,19 @@ export default {
           console.log("GET_PROJECTS");
         }
       });
+    },
+    action1() {
+      console.log("action1");
+    },
+    addProject() {
+      this.movePage("/AddProject");
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
+.project-list {
+  display: flex;
+}
 </style>
