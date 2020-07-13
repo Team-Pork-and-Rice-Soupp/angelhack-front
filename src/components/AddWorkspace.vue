00af432a4512d55b78482c6a872b71da2932d06f
GUGIG <jhc060109@naver.com>
2020-07-13 21:37:35 +0900

    fix: 워크스페이스 추가 컴포넌트 작성중

---
+++ src/components/AddWorkspace.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="work-space-form">
+        <q-input v-model="title" label="title"/>
+        <q-input v-model="description" label="description" autogrow />
+        <add-members />
+    </div>
+</template>
+
+<script>
+import AddMembers from "./AddMembers.vue";
+
+export default {
+    name: "add-workspace",
+    components: {
+        AddMembers
+    },
+    data() {
+        return {
+            title: "",
+            description: "",
+            members: []
+        };
+    },
+    methods: {
+        addingMember() {
+            this.$children
+                .find(el => el.vueName == "add-members")
+                .addMember(res => {
+                    this.members.push(res);
+                });
+        },
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+.work-space-form {
+    width: 100px;
+    .qinput {
+        width: 300px;
+    }
+}
+</style>
