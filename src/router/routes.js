353b766da9c94b14ba19066f5372adbc0a40e9c0
Moon Juhan <codemike@naver.com>
2020-07-13 21:04:21 +0900

    feat - 프로젝트 리스트 페이지 추가

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,6 +7,11 @@ const routes = [
       { path: "", component: () => import('pages/SignIn.vue') }
     ]
   },
+  {
+    path: '/ProjectList',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: "", component: () => import('pages/ProjectList.vue') }
     ]
   },
 
