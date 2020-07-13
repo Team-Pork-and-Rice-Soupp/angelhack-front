42befba6c24b130b47322e27451ee6fb8500dfee
JHC <jhc060109@naver.com>
2020-07-14 03:01:03 +0900

    fix: 워크스페이스카드 내부동작 및 css 수정

--- src/components/WorkspaceCard.vue
+++ src/components/WorkspaceCard.vue
@@ -1,48 +1,80 @@
 <template>
-    <q-card @click="clickAction" class="worksapce-card">
-        <q-card-section>
-            <h1>
-                <slot name="card-title"></slot>
-            </h1>
-        </q-card-section>
+  <q-card @click="clickAction" class="workspace-card">
+    <div v-if="!addButton" class="workspace-card__contents">
+      <q-card-section>
+        <h1>
+          <slot name="card-title"></slot>
+        </h1>
+      </q-card-section>
 
-        <q-card-section>
-            <p>
-                <slot name="card-description"></slot>
-            </p>
-        </q-card-section>
+      <q-card-section>
+        <p>
+          <slot name="card-description"></slot>
+        </p>
+      </q-card-section>
+    </div>
 
-    </q-card>
+    <div v-if="addButton === true" class="workspace-card__contents">
+      <q-card-section>
+        <div class="add-card">
+          <slot name="add-card"></slot>
+        </div>
+      </q-card-section>
+    </div>
+  </q-card>
 </template>
 
 <script>
 export default {
-    name: "workspace-card",
-    props: ["clickAction"],
-    data() {
-        return {};
-    },
-    methods: {}
+  name: "workspace-card",
+  props: ["workspaceId", "addButton"],
+  data() {
+    return {};
+  },
+  methods: {
+    clickAction() {
+      if(this.addButton) {
+        this.$router.push('/AddProject')
+      } else {
+        alert(`this workspace's id >> ${this.workspaceId}`);
+      }
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.worksapce-card {
-    width: 100%;
-    max-width: 250px;
-    height: 100%;
-    max-height: 250px;
-    margin-right: 25px;
-    background-color: #AAE0FF;
-    color: white;
-    &:hover {
-        cursor: pointer;
-    }
-    h1 {
-        font-size: 30px;        
-    }
-    p {
-        font-size: 10px;
-    }
+.workspace-card {
+  // width: 100%;
+  // height: 100%;
+  // max-width: 250px;
+  // max-height: 250px;
+  width: 250px;
+  height: 250px;
+  margin: 25px;
+  background-color: #aae0ff;
+  color: white;
+  display: flex;
+  &:hover {
+    cursor: pointer;
+  }
+  h1 {
+    font-size: 30px;
+    font-weight: bold;
+  }
+  p {
+    font-size: 10px;
+  }
+  .add-card {
+    font-size: 60px;    
+    font-weight: bold;
+    text-align: center;
+  }
+  &__contents {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
 }
 </style>
