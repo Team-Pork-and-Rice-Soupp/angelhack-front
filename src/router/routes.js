556d3af883878c2b3f8d655ba8c4d8d5a8a60c5b
JHC <jhc060109@naver.com>
2020-07-15 04:30:12 +0900

    fix: 테스트용 라우트 추가

--- src/router/routes.js
+++ src/router/routes.js
@@ -22,6 +22,13 @@ const routes = [
     ]
   },
   {
+    path: '/AddMeetingLog',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: "", component: () => import('pages/AddMeetingLog.vue') }
+    ]
+  },
+  {
     path: '/MeetingLog',
     component: () => import('layouts/MainLayout.vue'),
     children: [
