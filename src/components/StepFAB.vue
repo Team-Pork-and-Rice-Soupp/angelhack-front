382e0ed55d1be600b25d2e25bbd9c247f0d395c6
Moon Juhan <codemike@naver.com>
2020-07-14 21:56:28 +0900

    feat - 스텝 플로팅 버튼 컴포넌트 추가

---
+++ src/components/StepFAB.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="step-div">
+    <div class="step-div__title">
+      <q-fab
+        :color="stepData.locked ? 'grey' : 'blue'"
+        icon="keyboard_arrow_down"
+        direction="down"
+        :disable="stepData.locked"
+      >
+        <q-fab-action
+          external-label
+          color="primary"
+          @click="addStep(title)"
+          icon="add"
+          label="추가하기"
+        />
+
+        <q-fab-action
+          v-for="(item, index) in stepData.writed"
+          :key="index"
+          external-label
+          color="primary"
+          @click="item.click"
+          icon="article"
+          :label="item.label"
+        />
+      </q-fab>
+      <span class="title">{{title}}</span>
+    </div>
+    <span class="contents">{{description}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["title", "description", "stepData", "addStep"],
+  methods: {
+    onClick() {
+      console.log("click");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.step-div {
+  padding: 0 10px;
+  &__title {
+    display: flex;
+    align-items: center;
+    .q-fab {
+      width: 56px;
+      height: 56px;
+    }
+
+    .title {
+      margin-left: 10px;
+      height: fit-content;
+      font-size: 20px;
+      font-weight: bold;
+    }
+  }
+  .contents {
+    margin-left: 66px;
+  }
+}
+</style>
