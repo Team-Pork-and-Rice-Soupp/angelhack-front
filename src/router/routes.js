7e12d83ffdda35b82b17f14a82ef2e2c6e9b7948
Moon Juhan <codemike@naver.com>
2020-07-14 21:55:57 +0900

    feat - 대시보드 페이지 추가

--- src/router/routes.js
+++ src/router/routes.js
@@ -22,6 +22,15 @@ const routes = [
     ]
   },
 
+
+  {
+    path: '/Dashboard',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: "", component: () => import('pages/Dashboard.vue') }
+    ]
+  },
+
   // Always leave this as last one,
   // but you can also remove it
   {
