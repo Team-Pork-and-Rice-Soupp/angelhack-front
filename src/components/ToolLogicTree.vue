16d7570dab1db85dc40cfaf9fba487c33e517d5f
Moon Juhan <codemike@naver.com>
2020-07-15 22:02:23 +0900

    feat - 로직트리 컴포넌트 추가

---
+++ src/components/ToolLogicTree.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-for="(block, index) in logicTree" :key="index">
+      <div>
+        <div v-for="(node, index) in block.node" :key="index">{{node.label}}</div>
+      </div>
+      <q-btn label="Add" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["logicTree"]
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
