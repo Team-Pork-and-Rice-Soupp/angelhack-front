eb7fc104543031e447dde62b4714726dd7807e3d
GUGIG <jhc060109@naver.com>
2020-07-15 22:06:17 +0900

    fix: 스타일 전역적 적용을 위해 app.vue로 옮김.
    템플릿 선택 시 카드들 스타일임.

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,32 @@ export default {
   name: "App"
 };
 </script>
+
+<style lang="scss">
+.meeting-log {
+  &__dialog {
+    width: 1500px;
+    .card-container {
+      display: flex;
+      justify-content: space-between;
+      &__card {
+        width: 400px;
+        height: 400px;
+        padding: 20px;
+        transition: all 200ms ease;
+        &:hover {
+          filter: brightness(95%);
+          cursor: pointer;
+        }
+      }
+      &__foot {
+        display: flex;
+        justify-content: space-between;
+      }
+    }
+    span {
+      font-size: 20px;
+    }
+  }
+}
+</style>
