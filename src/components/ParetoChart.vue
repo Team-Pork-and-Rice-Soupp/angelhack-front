2660ecd06ff9d44937b22d2eb244367d3994af85
JHC <jhc060109@naver.com>
2020-07-17 05:17:40 +0900

    fix: 파레토 차트 구현
    기능동작 100% 완료

--- src/components/ParetoChart.vue
+++ src/components/ParetoChart.vue
@@ -1,276 +1,323 @@
 <template>
-    <div class="pareto-chart">
-        <div>
-            <q-table title="Treats" :data="data" :columns="columns" row-key="name" class="table">
-                <template v-slot:header="props">
-                    <q-tr :props="props">
-                        <!-- <q-th auto-width /> -->
-                        <q-th
-                            v-for="col in props.cols"
-                            :key="col.name"
-                            :props="props"
-                        >{{ col.label }}</q-th>
-                    </q-tr>
-                </template>
+  <div class="pareto-chart">
+    <div>
+      <div v-if="!tableEnabled">
+        <q-input label="비교 항목" v-model="scaleOption.xScaleLabel" />
+        <q-input label="데이터 단위" v-model="scaleOption.yScaleLabel" />
+        <q-btn label="표 생성" @click="enableTable" />
+      </div>
 
-                <template v-slot:body="props">
-                    <!-- original row -->
-                    <q-tr :props="props">
-                        <!-- (td)버튼 외 나머지 실제 데이터 -->
-                        <q-td
-                            v-for="col in props.cols"
-                            :key="col.name"
-                            :props="props"
-                        >{{ col.value }}</q-td>
-                    </q-tr>
+      <div v-if="tableEnabled">
+        <q-table title="데이터" :data="data" :columns="columns" row-key="number" selection="multiple"  class="table"> <!--row-key="name-->
+          <template v-slot:header="props">
+            <q-tr :props="props">
+              <q-th v-for="col in props.cols" :key="col.name" :props="props">{{ col.label }}</q-th> <!--column이기 때문에 name이 키가 맞음.-->
+            </q-tr>
+          </template>
 
-                    <!-- expanded row -->
-                    <q-tr v-show="props.expand" :props="props">
-                        <!-- (td)버튼 -->
-                        <q-td auto-width>
-                            <q-btn
-                                size="sm"
-                                color="accent"
-                                round
-                                dense
-                                @click="props.expand = !props.expand"
-                                :icon="props.expand ? 'remove' : 'add'"
-                            />
-                        </q-td>
+          <template v-slot:body="props">
+            <q-tr :props="props">
+              <q-td key="number" :props="props">{{ props.row.number }}</q-td>
+              <q-td key="item" :props="props">{{ props.row.item }}</q-td>
+              <q-td key="quantity" :props="props">{{ props.row.quantity }}</q-td>
+              <q-td><q-btn label="삭제" color="negative" @click="deleteRow(props.row)"/></q-td>
+            </q-tr>
+          </template>
 
-                        <!-- (td)버튼 외 나머지 실제 데이터 -->
-                        <q-td
-                            v-for="col in props.cols"
-                            :key="col.name"
-                            :props="props"
-                        >{{ col.value }}</q-td>
-                    </q-tr>
-                </template>
-            </q-table>
-            <q-input v-model="newData.product" label="제품" />
-            <q-input v-model="newData.sale" label="판매수량" />
-            <q-btn label="데이터 추가" @click="addNewRow" />
-        </div>
-
-        <pareto-chart :chartData="realtimeData" :options="options" class="small" :key="refreshChart" />
+        </q-table>
+        <q-input v-model="newData.item" :label="scaleOption.xScaleLabel" />
+        <q-input v-model="newData.quantity" type="number" :label="scaleOption.yScaleLabel" />
+        <q-btn label="데이터 추가" @click="addNewRow" />
+      </div>
     </div>
+    <pareto-chart :chartData="datacollection" :options="options" class="small" :key="refreshChart" />
+  </div>
 </template>
 
 <script>
 import ParetoChart from "../components/BarChart";
 export default {
-    name: "pareto",
-    components: {
-        ParetoChart
-    },
-    data() {
-        return {
-            refreshChart: 1,
-            newData: {
-                name: "",
-                product: "",
-                sale: ""
-            },
-            columns: [
-                {
-                    name: "desc",
-                    required: true,
-                    label: "번호",
-                    align: "center",
-                    field: row => row.name,
-                    format: val => `${val}`,
-                    sortable: true
-                },
-                {
-                    name: "product",
-                    align: "center",
-                    label: "제품",
-                    field: "product",
-                    sortable: true
-                },
-                {
-                    name: "sale",
-                    label: "판매수량",
-                    field: "sale",
-                    sortable: true,
-                    style: "width: 10px"
-                }
-            ],
-            data: [
-                {
-                    name: "1",
-                    product: "A 제품",
-                    sale: 6.0
-                },
-                {
-                    name: "2",
-                    product: "B 제품",
-                    sale: 9.0
-                },
-                {
-                    name: "3",
-                    product: "C 제품",
-                    sale: 16.0
-                },
-                {
-                    name: "4",
-                    product: "D 제품",
-                    sale: 3
-                },
-                {
-                    name: "5",
-                    product: "E 제품",
-                    sale: 16.0
-                }
-            ],
-            datacollection: {
-                labels: [],
-                datasets: [
-                    {
-                        type: "line",
-                        label: "누적",
-                        borderColor: "#BA1E14",
-                        backgroundColor: "#BA1E14",
-                        pointBorderWidth: 5,
-                        fill: false,
-                        data: [],
-                        yAxisID: "y-axis-2"
-                    },
-                    {
-                        type: "bar",
-                        label: "판매수량",
-                        borderColor: "#56B513",
-                        backgroundColor: "#56B513",
-                        data: [],
-                        yAxisID: "y-axis-1"
-                    },
-                ]
-            },
-            options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                scales: {
-                    xAxes: [
-                        {
-                            stacked: true,
-                            scaleLabel: {
-                                display: true,
-                                labelString: "Estaciones"
-                            },
-                            barPercentage: 0.5
-                        }
-                    ],
+  name: "pareto",
+  components: {
+    ParetoChart
+  },
+  // mounted() {
 
-                    yAxes: [
-                        {
-                            type: "linear",
-                            position: "left",
-                            id: "y-axis-1",
-                            stacked: true,
-                            ticks: {
-                                suggestedMin: 0
-                            },
-                            scaleLabel: {
-                                display: true,
-                                labelString: "Minutos"
-                            }
-                        },
-                        {
-                            type: "linear",
-                            position: "right",
-                            id: "y-axis-2",
-                            ticks: {
-                                suggestedMin: 0,
-                                callback: function(value) {
-                                    return value + "%";
-                                }
-                            },
-                            scaleLabel: {
-                                display: true,
-                                labelString: "%"
-                            }
-                        }
-                    ]
-                }
-            }
-            // datacollection: {
-            //     labels: ["8", "7", "9", "11", "10"],
-            //     datasets: [
-            //         {
-            //             type: "line",
-            //             label: "누적",
-            //             borderColor: "#BA1E14",
-            //             backgroundColor: "#BA1E14",
-            //             pointBorderWidth: 5,
-            //             fill: false,
-            //             data: [34.04, 57.45, 76.6, 89.36, 100.0],
-            //             yAxisID: "y-axis-2"
-            //         },
-            //         {
-            //             type: "bar",
-            //             label: "Asistencia",
-            //             borderColor: "#56B513",
-            //             backgroundColor: "#56B513",
-            //             data: [16, 11, 9, 6, 5],
-            //             yAxisID: "y-axis-1"
-            //         },
-            //         {
-            //             type: "bar",
-            //             label: "Solución",
-            //             borderColor: "#000FAA",
-            //             backgroundColor: "#000FAA",
-            //             data: [16, 11, 9, 6, 5],
-            //             yAxisID: "y-axis-1"
-            //         }
-            //     ]
-            // },
-        };
-    },
-    computed: {
-        realtimeData() {
-            this.data.forEach(datum => {
-                this.datacollection.labels.push(datum.product);
-                this.datacollection.datasets[1].data.push(datum.sale);
-            });
-            return this.datacollection;
-            // this.datacollection.labels = this.data
-        }
-    },
-    methods: {
-        addNewRow() {
-            this.newData.name = this.data.length + 1;
-            this.data.push(this.newData);
-            this.datacollection.labels.push(this.newData.product);
-            this.datacollection.datasets[1].data.push(this.newData.sale);
-            this.newData = {
-                name: "",
-                product: "",
-                sale: ""
-            };
-            this.refreshChart *= -1;
+  // }
+  data() {
+    return {
+      tableEnabled: false,
+      scaleOption: {
+        xScaleLabel: "",
+        xScaleLabel: ""
+      },
+
+      refreshChart: 1,
+      rawDataList: [],
+      newData: {
+        number: "",
+        item: "",
+        quantity: ""
+      },
+      columns: [
+        {
+          name: "number",
+          required: true,
+          label: "번호",
+          align: "center",
         },
-        isLastRow(row) {
-            if (row == this.data[this.data.length - 1]) {
-                return true;
-            }
-            false;
+        {
+          name: "item",
+          align: "center",
+          label: "항목",
+          field: "item",
+        },
+        {
+          name: "quantity",
+          label: "수량",
+          field: "quantity",
+        }, 
+        {
+          name: "deleteButton",
+          align: "center",
+          label: "",
+          field: "deleteButton"
         }
+      ],
+      data: [
+        // {
+        //     name: "1",
+        //     product: "A 제품",
+        //     sale: 6.0
+        // },
+        // {
+        //     name: "2",
+        //     product: "B 제품",
+        //     sale: 9.0
+        // },
+        // {
+        //     name: "3",
+        //     product: "C 제품",
+        //     sale: 16.0
+        // },
+        // {
+        //     name: "4",
+        //     product: "D 제품",
+        //     sale: 3
+        // },
+        // {
+        //     name: "5",
+        //     product: "E 제품",
+        //     sale: 16.0
+        // }
+      ],
+      datacollection: {
+        labels: [],
+        datasets: [
+          {
+            type: "line",
+            label: "누적",
+            borderColor: "#BA1E14",
+            backgroundColor: "#BA1E14",
+            pointBorderWidth: 5,
+            fill: false,
+            data: [], // 34.04, 57.45, 76.6, 89.36, 100.0 just in case you'd want to test line chart.
+            yAxisID: "y-axis-2"
+          },
+          {
+            type: "bar",
+            label: "판매수량",
+            borderColor: "#56B513",
+            backgroundColor: "#56B513",
+            data: [],
+            yAxisID: "y-axis-1"
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [
+            {
+              stacked: true,
+              scaleLabel: {
+                display: true,
+                labelString: "비교항목"
+              },
+              barPercentage: 0.5
+            }
+          ],
+
+          yAxes: [
+            {
+              type: "linear",
+              position: "left",
+              id: "y-axis-1",
+              stacked: true,
+              ticks: {
+                suggestedMin: 0
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "데이터 단위"
+              }
+            },
+            {
+              type: "linear",
+              position: "right",
+              id: "y-axis-2",
+              ticks: {
+                suggestedMin: 0,
+                callback: function(value) {
+                  return value + "%";
+                }
+              },
+              scaleLabel: {
+                display: true,
+                labelString: "%"
+              }
+            }
+          ]
+        }
+      }
+      // datacollection: {
+      //     labels: ["8", "7", "9", "11", "10"],
+      //     datasets: [
+      //         {
+      //             type: "line",
+      //             label: "누적",
+      //             borderColor: "#BA1E14",
+      //             backgroundColor: "#BA1E14",
+      //             pointBorderWidth: 5,
+      //             fill: false,
+      //             data: [34.04, 57.45, 76.6, 89.36, 100.0],
+      //             yAxisID: "y-axis-2"
+      //         },
+      //         {
+      //             type: "bar",
+      //             label: "Asistencia",
+      //             borderColor: "#56B513",
+      //             backgroundColor: "#56B513",
+      //             data: [16, 11, 9, 6, 5],
+      //             yAxisID: "y-axis-1"
+      //         },
+      //         {
+      //             type: "bar",
+      //             label: "Solución",
+      //             borderColor: "#000FAA",
+      //             backgroundColor: "#000FAA",
+      //             data: [16, 11, 9, 6, 5],
+      //             yAxisID: "y-axis-1"
+      //         }
+      //     ]
+      // },
+    };
+  },
+  // computed: {
+  //     realtimeData() {
+  //         this.data.forEach(datum => {
+  //             this.datacollection.labels.push(datum.product);
+  //             this.datacollection.datasets[1].data.push(datum.sale);
+  //         });
+  //         return this.datacollection;
+  //         // this.datacollection.labels = this.data
+  //     }
+  // },
+  methods: {
+    deleteRow(row) {
+      let index = this.data.findIndex(datum => datum.number == row.number);
+      let datum = this.data[index];
+
+      let labelIndex = this.datacollection.labels.findIndex(label => label == datum.item);
+      let dataIndex = this.datacollection.datasets[1].data.findIndex(datm => datm == datum.quantity);
+
+      this.datacollection.labels.splice(labelIndex, 1);
+      this.datacollection.datasets[1].data.splice(dataIndex, 1);
+
+      this.datacollection.datasets[0].data = [];
+      let sum = this.datacollection.datasets[1].data.reduce((acc, cur) => parseInt(acc) + parseInt(cur)); 
+      console.log("sum >> ", sum);
+      let accPercent = 0;
+      this.datacollection.datasets[1].data.forEach(datum => {
+          let percent = datum / sum * 100;
+          accPercent += percent;
+          console.log("percent >> ", percent);
+          console.log("accPercent >> ", accPercent);
+          this.datacollection.datasets[0].data.push(accPercent);          
+      });
+
+      this.data.splice(index, 1);
+      
+      this.refreshChart *= -1;
+    },
+    enableTable() {      
+      this.options.scales.xAxes[0].scaleLabel.labelString = this.scaleOption.xScaleLabel;
+      this.options.scales.yAxes[0].scaleLabel.labelString = this.scaleOption.yScaleLabel;
+      this.columns[1].label = this.scaleOption.xScaleLabel;
+      this.columns[2].label = this.scaleOption.yScaleLabel;
+      this.refreshChart *= -1;
+      this.tableEnabled = true;
+    },
+    addNewRow() {
+      this.newData.number = this.data.length + 1;
+      this.data.push(this.newData);
+
+      let tempList = [...this.data];
+      tempList.sort((a, b) => {
+        // desc sort
+        if (parseInt(a.quantity) > parseInt(b.quantity)) return -1;
+        else if (parseInt(a.quantity) == parseInt(b.quantity)) return 0;
+        else if (parseInt(a.quantity) < parseInt(b.quantity)) return 1;
+      });
+      this.datacollection.labels = [];
+      this.datacollection.datasets[0].data = [];
+      this.datacollection.datasets[1].data = [];
+      tempList.forEach(temp => {
+        this.datacollection.labels.push(temp.item);
+        this.datacollection.datasets[1].data.push(temp.quantity);
+      });
+
+      let sum = this.datacollection.datasets[1].data.reduce((acc, cur) => parseInt(acc) + parseInt(cur)); 
+      console.log("sum >> ", sum);
+      let accPercent = 0;
+      this.datacollection.datasets[1].data.forEach(datum => {
+          let percent = datum / sum * 100;
+          accPercent += percent;
+          console.log("percent >> ", percent);
+          console.log("accPercent >> ", accPercent);
+          this.datacollection.datasets[0].data.push(accPercent);          
+      });
+
+      this.newData = {
+        number: "",
+        item: "",
+        quantity: ""
+      };
+      this.refreshChart *= -1;
+    },
+    isLastRow(row) {
+      if (row == this.data[this.data.length - 1]) {
+        return true;
+      }
+      false;
     }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .pareto-chart {
-    display: flex;
-    justify-content: space-around;
-    .table {
-        width: 350px;
-    }
-    .small {
-        width: 900px;
-        height: 500px;
-        max-height: 500px;
-        max-width: 1500px;
-    }
+  display: flex;
+  justify-content: space-around;
+  .table {
+    width: 400px;
+  }
+  .small {
+    width: 900px;
+    height: 500px;
+    max-height: 500px;
+    max-width: 1500px;
+  }
 }
 </style>
