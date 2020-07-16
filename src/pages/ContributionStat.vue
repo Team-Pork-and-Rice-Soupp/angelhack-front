42db5d428d2290d38400186074f3f0f146b65c94
GUGIG <jhc060109@naver.com>
2020-07-16 19:39:54 +0900

    fix: 차트 컴포넌트 이름변경

--- src/pages/ContributionStat.vue
+++ src/pages/ContributionStat.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import Chart from "../components/Chart.vue";
+import Chart from "../components/BarChart.vue";
 export default {
   name: "contribution-stat",
   components: {
