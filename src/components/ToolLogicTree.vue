60b07f8249f0d81c47289c3efa3abeff88c1dc1c
Moon Juhan <codemike@naver.com>
2020-07-16 21:39:35 +0900

    feat - 로직 트리 도구 완료

--- src/components/ToolLogicTree.vue
+++ src/components/ToolLogicTree.vue
@@ -2,13 +2,31 @@
   <div class="logic-tree">
     <div v-for="(block, index) in logicTree" :key="index" class="logic-tree__block">
       <div class="node-wrapper">
-        <div class="node" v-for="(node, index) in block.node" :key="index">
+        <div
+          class="node"
+          v-for="(node, nodeIndex) in block.node"
+          :key="nodeIndex"
+          :ref="index + 'and' + nodeIndex"
+        >
           <span>{{node.label}}</span>
-          <q-btn label="삭제" @click="onClickDelete(block,index)" />
+          <span style="display:none">{{node.pre}}</span>
+          <q-btn size="8px" label="삭제" @click="onClickDelete(block,index, nodeIndex)" />
         </div>
       </div>
       <q-btn label="Add" @click="onClickAddNode(block, index)" />
     </div>
+
+    <svg>
+      <line
+        :x1="item.left - 10"
+        :y1="item.top - 140"
+        :x2="item.preRight + 10"
+        :y2="item.preTop - 140"
+        stroke="black"
+        v-for="(item, index) in svgList"
+        :key="index"
+      />
+    </svg>
 
     <!-- Dialog -->
     <q-dialog v-model="dialogOpened" @hide="hideDialog">
@@ -55,7 +73,9 @@ export default {
         {
           node: []
         }
-      ]
+      ],
+      svgList: [],
+      updateCheck: false
     };
   },
   methods: {
@@ -91,7 +111,7 @@ export default {
         alert("이전 노드를 선택하세요.");
       }
 
-      console.log(this.dialogInput);
+      this.drawLine();
     },
     onClickSelect(label) {
       if (this.selected == label) {
@@ -100,11 +120,65 @@ export default {
         this.selected = label;
       }
     },
+    onClickDelete(block, index, nodeIndex) {
+      for (var i = this.logicTree[index + 1].node.length - 1; i >= 0; i--) {
+        if (
+          this.logicTree[index + 1].node[i].pre ==
+          this.logicTree[index].node[nodeIndex].label
+        ) {
+          this.onClickDelete(this.logicTree[index + 1], index + 1, i);
+        }
+      }
+      this.logicTree[index].node.splice(nodeIndex, 1);
+
+      if (this.logicTree[index].node.length == 0 && this.logicTree[index + 1]) {
+        this.logicTree.splice(index + 1, 1);
+      }
+      this.drawLine();
+    },
     hideDialog() {
       this.dialogData = {};
       this.dialogIndex = 0;
       this.dialogInput = "";
       this.selected = "";
+    },
+    drawLine() {
+      this.svgList = [];
+      setTimeout(() => {
+        let lineList = [];
+
+        for (const [key, value] of Object.entries(this.$refs)) {
+          console.log(value);
+          if (value.length != 0) {
+            let pushObj = {
+              top: value[0].getBoundingClientRect().top,
+              bottom: value[0].getBoundingClientRect().bottom,
+              left: value[0].getBoundingClientRect().left,
+              right: value[0].getBoundingClientRect().right,
+              label: value[0].childNodes[0].innerHTML,
+              pre: value[0].childNodes[1].innerHTML
+            };
+            lineList.push(pushObj);
+          }
+        }
+
+        lineList.forEach((node, index) => {
+          lineList.forEach((_node, _index) => {
+            if (index != _index && node.label == _node.pre) {
+              this.svgList.push({
+                preTop: node.top,
+                preBottom: node.bottom,
+                preLeft: node.left,
+                preRight: node.right,
+                top: _node.top,
+                bottom: _node.bottom,
+                left: _node.left,
+                right: _node.right
+              });
+            }
+          });
+        });
+      }, 50);
     }
   }
 };
@@ -122,9 +196,22 @@ export default {
       .node {
         border: 1px solid black;
         display: flex;
-        justify-content: center;
+        justify-content: space-between;
+        margin-bottom: 10px;
+        align-items: center;
+        padding: 6px;
+        span {
+          margin-right: 10px;
+        }
       }
     }
+  }
+
+  svg {
+    position: absolute;
+    width: 100%;
+    height: 50%;
+    z-index: -10;
   }
 }
 </style>
