a3b9a0e5461f2dc263086bb05ee5b46ae0e06a7b
Moon Juhan <codemike@naver.com>
2020-07-17 20:29:48 +0900

    feat - asis tobe 도구 개발

--- src/pages/CreateTools.vue
+++ src/pages/CreateTools.vue
@@ -109,8 +109,61 @@
       <div v-else-if="title == '아이디어 도출'">
         <span>개발중 입니다.</span>
       </div>
-      <div v-else-if="title == 'ASIS-TOBE'">
-        <span>개발중 입니다.</span>
+      <div v-else-if="title == 'ASIS-TOBE'" class="asis-tobe">
+        <div class="asis-tobe__card">
+          <div class="card-title">
+            <span>AS IS</span>
+          </div>
+          <div class="card-contents">
+            <div class="card-contents__input">
+              <q-input filled v-model="asisTobe.asisInput" />
+              <q-btn
+                label="입력"
+                @click="onClickAsisTobe(asisTobe.asisList, asisTobe.asisInput, 'asisInput')"
+              />
+            </div>
+            <div v-for="(item, index) in asisTobe.asisList" :key="index">
+              <span>{{item}}</span>
+              <q-btn label="삭제" @click="onClickDeleteAT(asisTobe.asisList, index)" />
+            </div>
+          </div>
+        </div>
+        <div class="asis-tobe__keyword">
+          <div class="card-title">
+            <span>Keyword</span>
+          </div>
+          <div class="card-contents">
+            <div class="card-contents__input">
+              <q-input filled v-model="asisTobe.keywordInput" />
+              <q-btn
+                label="입력"
+                @click="onClickAsisTobe(asisTobe.keywordList, asisTobe.keywordInput, 'keywordInput')"
+              />
+            </div>
+            <div v-for="(item, index) in asisTobe.keywordList" :key="index">
+              <span>{{item}}</span>
+              <q-btn label="삭제" @click="onClickDeleteAT(asisTobe.keywordList, index)" />
+            </div>
+          </div>
+        </div>
+        <div class="asis-tobe__card">
+          <div class="card-title">
+            <span>TO BE</span>
+          </div>
+          <div class="card-contents">
+            <div class="card-contents__input">
+              <q-input filled v-model="asisTobe.tobeInput" />
+              <q-btn
+                label="입력"
+                @click="onClickAsisTobe(asisTobe.tobeList, asisTobe.tobeInput, 'tobeInput')"
+              />
+            </div>
+            <div v-for="(item, index) in asisTobe.tobeList" :key="index">
+              <span>{{item}}</span>
+              <q-btn label="삭제" @click="onClickDeleteAT(asisTobe.tobeList, index)" />
+            </div>
+          </div>
+        </div>
       </div>
       <div v-else-if="title == 'Time Line'">
         <span>개발중 입니다.</span>
@@ -235,7 +288,15 @@ export default {
           label: "Why",
           input: ""
         }
-      ]
+      ],
+      asisTobe: {
+        asisInput: "",
+        asisList: [],
+        tobeInput: "",
+        tobeList: [],
+        keywordInput: "",
+        keywordList: []
+      }
     };
   },
   mounted() {
@@ -352,6 +413,13 @@ export default {
       } else {
         pest.data.push({ input: "" });
       }
+    },
+    onClickAsisTobe(list, item, key) {
+      list.push(item);
+      this.asisTobe[key] = "";
+    },
+    onClickDeleteAT(list, index) {
+      list.splice(index, 1);
     }
   }
 };
@@ -496,6 +564,51 @@ export default {
 
       &__form {
         width: 80%;
+      }
+    }
+
+    .asis-tobe {
+      display: flex;
+      &__card,
+      &__keyword {
+        width: 30%;
+
+        .card-title {
+          display: flex;
+          justify-content: center;
+          margin-bottom: 10px;
+          span {
+            font-size: 16px;
+            font-weight: bold;
+          }
+        }
+
+        .card-contents {
+          div {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            border-bottom: 1px solid #babaab;
+            margin-bottom: 10px;
+            padding-bottom: 10px;
+            label,
+            span {
+              width: 68%;
+            }
+            button {
+              width: 30%;
+              height: 100%;
+            }
+          }
+
+          &__input {
+            border-bottom: 1px solid black !important;
+          }
+        }
+      }
+      &__keyword {
+        padding: 20px;
+        width: 40%;
       }
     }
   }
